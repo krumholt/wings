9f1f6410d2fdd52acfb8196a3a19d4d6f8be6bba
Tobias Krumholz <krumholt@gmail.com>
2023-08-08 11:00:43 +0200

    >..

--- mem.c
+++ mem.c
@@ -24,17 +24,23 @@ struct memory_block_stack
     u64 used;
 };
 
-struct stack_allocator
+struct linear_growing_allocator
 {
     struct memory_block_stack *memory_block;
 
-	u64 number_of_blocks;
+    u64 number_of_blocks;
     u64 min_block_size;
+};
+
+struct linear_fixed_size_allocator
+{
+    struct memory_block_stack memory_block;
 };
 
 enum allocator_type
 {
-    allocator_type_stack_allocator,
+    allocator_type_linear_growing_allocator,
+    allocator_type_linear_fixed_size_allocator,
 };
 
 struct allocator
@@ -43,26 +49,27 @@ struct allocator
     s32                 alignment;
     union
     {
-        struct stack_allocator stack_allocator;
+        struct linear_growing_allocator    linear_growing_allocator;
+        struct linear_fixed_size_allocator linear_fixed_size_allocator;
     } allocator;
 };
 
 struct allocator
-make_stack_allocator(void)
+make_linear_growing_allocator(void)
 {
     struct allocator allocator = {
-        .type            = allocator_type_stack_allocator,
-        .alignment       = 8,
-        .allocator.stack_allocator = {
-			.memory_block = 0,
-			.min_block_size = 1024 * 1024,
-		},
+        .type                               = allocator_type_linear_growing_allocator,
+        .alignment                          = 8,
+        .allocator.linear_growing_allocator = {
+                                               .memory_block   = 0,
+                                               .min_block_size = 1024 * 1024,
+                                               },
     };
     return (allocator);
 }
 
 u8 *
-stack_allocator_allocate(struct stack_allocator *allocator, u64 alignment, u64 size)
+linear_growing_allocator_allocate(struct linear_growing_allocator *allocator, u64 alignment, u64 size)
 {
     size = (size + alignment - 1) & ~(alignment - 1);
     if (!allocator->memory_block
@@ -72,16 +79,45 @@ stack_allocator_allocate(struct stack_allocator *allocator, u64 alignment, u64 s
 
         struct memory_block_stack *new_block
             = calloc(1, sizeof(struct memory_block_stack));
-		new_block->size = new_block_size;
-		new_block->base = calloc(new_block_size, sizeof(u8));
+        new_block->size = new_block_size;
+        new_block->base = calloc(new_block_size, sizeof(u8));
 
-		new_block->previous = allocator->memory_block;
-		allocator->memory_block = new_block;
-		allocator->number_of_blocks +=  1;
+        new_block->previous     = allocator->memory_block;
+        allocator->memory_block = new_block;
+        allocator->number_of_blocks += 1;
     }
 
-    u8 *memory       = allocator->memory_block->base + allocator->memory_block->used;
+    u8 *memory = allocator->memory_block->base + allocator->memory_block->used;
     allocator->memory_block->used += size;
+    return (memory);
+}
+
+struct allocator
+make_linear_fixed_size_allocator(u64 size)
+{
+    struct allocator allocator = {
+        .type      = allocator_type_linear_fixed_size_allocator,
+        .alignment = 8,
+        .allocator.linear_fixed_size_allocator
+            .memory_block.base
+        = calloc(size, 1),
+        .allocator.linear_fixed_size_allocator
+            .memory_block.size
+        = size,
+    };
+    return (allocator);
+}
+
+u8 *
+linear_fixed_size_allocator_allocate(struct linear_fixed_size_allocator *allocator, u64 alignment, u64 size)
+{
+    size            = (size + alignment - 1) & ~(alignment - 1);
+    u64 memory_left = allocator->memory_block.size - allocator->memory_block.used;
+    assert(memory_left >= size);
+
+    u8 *memory = allocator->memory_block.base
+                 + allocator->memory_block.used;
+    allocator->memory_block.used += size;
     return (memory);
 }
 
@@ -90,32 +126,45 @@ allocate(struct allocator *allocator, u64 size)
 {
     switch (allocator->type)
     {
-    case allocator_type_stack_allocator:
-    {
-        return stack_allocator_allocate(&allocator->allocator.stack_allocator,
-                                        allocator->alignment, size);
-    }
-    break;
+    case allocator_type_linear_growing_allocator:
+        return linear_growing_allocator_allocate(
+            &allocator->allocator.linear_growing_allocator,
+            allocator->alignment, size);
+    case allocator_type_linear_fixed_size_allocator:
+        return linear_fixed_size_allocator_allocate(
+            &allocator->allocator.linear_fixed_size_allocator,
+            allocator->alignment, size);
     }
 }
 
 inline void
-stack_allocator_free_top_block(struct stack_allocator *allocator)
+linear_growing_allocator_free_top_block(struct linear_growing_allocator *allocator)
 {
-	struct memory_block_stack *top = allocator->memory_block;
-	allocator->memory_block = top->previous;
-	allocator->number_of_blocks -= 1;
-	free(top);
+    struct memory_block_stack *top = allocator->memory_block;
+    allocator->memory_block        = top->previous;
+    allocator->number_of_blocks -= 1;
+    free(top);
 }
 
 void
-stack_allocator_clear(struct stack_allocator *allocator)
+linear_growing_allocator_clear(struct linear_growing_allocator *allocator)
 {
-	while(allocator->memory_block)
-	{
-		stack_allocator_free_top_block(allocator);
-	}
+    while (allocator->memory_block)
+    {
+        linear_growing_allocator_free_top_block(allocator);
+    }
+}
 
+void
+linear_fixed_size_allocator_clear(struct linear_fixed_size_allocator *allocator)
+{
+    for (s32 index = 0;
+         index < allocator->memory_block.used;
+         ++index)
+    {
+        allocator->memory_block.base[index] = 0;
+    }
+	allocator->memory_block.used = 0;
 }
 
 void
@@ -123,9 +172,14 @@ allocator_clear(struct allocator *allocator)
 {
     switch (allocator->type)
     {
-    case allocator_type_stack_allocator:
+    case allocator_type_linear_growing_allocator:
     {
-        stack_allocator_clear(&allocator->allocator.stack_allocator);
+        linear_growing_allocator_clear(&allocator->allocator.linear_growing_allocator);
+    }
+    break;
+    case allocator_type_linear_fixed_size_allocator:
+    {
+        linear_fixed_size_allocator_clear(&allocator->allocator.linear_fixed_size_allocator);
     }
     break;
     }
