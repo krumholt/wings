6b406ff055b3631f2eed27ec259354a025d4cf32
Tobias Krumholz <krumholt@gmail.com>
2023-08-13 05:07:47 +0200

    ...

--- base/tests.c
+++ base/tests.c
@@ -1,7 +1,7 @@
-#ifndef TEST_C_
-#define TEST_C_
+#ifndef WINGS_BASE_TESTS_C_
+#define WINGS_BASE_TESTS_C_
 
-#include "base/types.h"
+#include "wings/base/types.c"
 
 #include <stdio.h>
 #include <string.h>
