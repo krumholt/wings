71c97b4131b4f970b9ff1cb8b596a0baf62427e0
Tobias Krumholz <krumholt@gmail.com>
2023-09-20 09:59:50 +0200

    ...

--- base/wings_file_reader.c
+++ base/wings_file_reader.c
@@ -4,24 +4,11 @@
 #include "wings/base/types.c"
 #include "wings/base/allocators.c"
 
-struct wings_file_parser
-{
-    struct buffer buffer;
-};
-
 struct wings_file_header
 {
     char id[8]; // "WINGS\0\0\0"
     u32  major_version; // 1
     u32  minor_version; // 0
-    u8   data[];
-};
-
-struct wings_file_chunk
-{
-    char id[16];
-    u64  size;
-    u64  number_of_child_blocks;
     u8   data[];
 };
 
@@ -40,6 +27,24 @@ struct wings_file_blender_chunk
     u32 blender_version;
 };
 
+struct wings_file_chunk
+{
+    char id[16];
+    u64  size;
+    u64  number_of_child_blocks;
+    u8   data[];
+};
+
+struct wings_file_parser
+{
+    union
+    {
+        u8                      *current;
+        struct wings_file_chunk *chunk;
+    };
+    u8 *end;
+};
+
 error
 make_wings_file_parser(struct wings_file_parser *file_parser, struct buffer buffer)
 {
@@ -55,67 +60,45 @@ make_wings_file_parser(struct wings_file_parser *file_parser, struct buffer buff
         return (1);
     if (header->major_version != 1 || header->minor_version != 0)
         return (2);
-    file_parser->buffer = buffer;
+    file_parser->current = buffer.base + sizeof(struct wings_file_header);
+    file_parser->end     = buffer.base + buffer.size;
+    return (0);
+}
+
+b32
+wings_file_id_equals(struct wings_file_chunk *chunk, const char *id)
+{
+
+    char *tmp = chunk->id;
+    while (*id++ && *tmp++)
+    {
+        if (*id != *tmp)
+            return (0);
+    }
+    return (1);
+}
+
+error
+set_to_next_chunk(struct wings_file_parser *parser)
+{
+    if (parser->current >= parser->end)
+        return (1);
+    struct wings_file_chunk *chunk = parser->chunk;
+    parser->current                = parser->current + chunk->size;
     return (0);
 }
 
 error
-set_to_next_model_chunk(struct wings_file_parser parser)
+set_to_next_chunk_with_id(struct wings_file_parser *parser, const char *id)
 {
-}
-
-struct wings_file_model_chunk *
-get_next_model_chunk(struct wings_file *file)
-{
-    u8 *file_pointer = file->data;
-    for (struct wings_file_chunk *chunk = (struct wings_file_chunk *)file_pointer;
-         (u64)(file_pointer - file->data.base) < file->data.size;
-         file_pointer += chunk->size)
+    while (set_to_next_chunk(parser) == NO_ERROR)
     {
+        if (wings_file_id_equals(parser->chunk, id))
+        {
+            return (0);
+        }
     }
-    printf("%s\n", file->id);
-    printf("version %d.%d\n", file->major_version, file->minor_version);
-
-    char                          *strings     = 0;
-    struct wings_file_model_chunk *model_chunk = 0;
-    chunk                                      = (struct wings_file_chunk *)file_pointer;
-    if (wings_file_id_equals(chunk, "blender"))
-    {
-        struct wings_file_blender_chunk *blender_chunk
-            = (struct wings_file_blender_chunk *)chunk->data;
-        char      tmp[32] = { 0 };
-        struct tm t       = { 0 };
-        time_t    in      = blender_chunk->creation_time;
-        localtime_s(&t, &in);
-        strftime(tmp, 32, "%H:%M %d.%m.%Y", &t);
-        printf("created at %s with %zu models from file %s with blender version %s\n",
-               tmp,
-               blender_chunk->number_of_models,
-               strings + blender_chunk->blend_file_name,
-               strings + blender_chunk->blender_version);
-    }
-    else if (wings_file_id_equals(chunk, "strings"))
-    {
-        strings = (char *)(file_pointer + sizeof(struct wings_file_chunk));
-    }
-    else if (wings_file_id_equals(chunk, "model"))
-    {
-        model_chunk = (struct wings_file_model_chunk *)(file_pointer + sizeof(struct wings_file_chunk));
-    }
-    else if (wings_file_id_equals(chunk, "mesh"))
-    {
-        printf("mesh found\n");
-    }
-    printf("chunk: %.16s\n", chunk->id);
-    printf("size: %zu\n", chunk->size);
-    printf("children: %zu\n", chunk->number_of_child_blocks);
-}
-
-printf("model chunk:\n");
-printf("name: %s\n", strings + model_chunk->name);
-printf("meshes: %d\n", model_chunk->number_of_meshes);
-
-return (0);
+    return (1);
 }
 
 #endif
