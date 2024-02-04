c1ae0229da864183b52b9de7821cf35208add76c
Tobias Krumholz <krumholt@gmail.com>
2024-02-05 00:45:30 +0100

    ..

--- base/macros.c
+++ base/macros.c
@@ -56,4 +56,45 @@
 #define FIRST_AND_COMMA(Token, ...) Token,
 #define FIRST_AS_STRING_AND_COMMA(Token, ...) #Token,
 
+
+#define DYNAMIC_ARRAY_HEADER(Name) \
+struct Name##_array\
+{\
+   s32 length;\
+   s32 capacity;\
+   struct Name *array;\
+};\
+struct Name##_array \
+Name##_array_make(s32 capacity);\
+\
+void \
+Name##_array_append(struct Name##_array *array, struct Name value)
+
+#define DYNAMIC_ARRAY_IMPLEMENTATION(Name) \
+struct Name##_array \
+Name##_array_make(s32 capacity)\
+{\
+   struct Name##_array array = {0};\
+   array.capacity = capacity;\
+   array.length = 0;\
+   array.array = calloc(array.capacity, sizeof(struct Name));\
+   ASSERT(array.array);\
+   return(array);\
+}\
+void\
+Name##_array_append(struct Name##_array *array, struct Name value)\
+{\
+   if (array->length == array->capacity)\
+   {\
+      struct Name *new_array = 0;\
+      s32 new_capacity = array->capacity * 2;\
+      new_array = calloc(array->capacity, sizeof(struct Name));\
+      ASSERT(new_array);\
+      memcpy(new_array, array->array, array->length * sizeof(struct Name));\
+      array->capacity = new_capacity;\
+      array->array = new_array;\
+   }\
+   array->array[array->length++] = value;\
+}
+
 #endif
